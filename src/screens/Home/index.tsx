1e05b4e529af93c34c35496e3a8ee5524cdcb312
Natanael Melo <109748470+Natanael135@users.noreply.github.com>
2025-03-14 14:39:22 -0300

    feat: implementando funçoes para deixar a adição e remoção de participantes de forma dinamica

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,42 @@
-import { Text, View, TextInput, TouchableOpacity } from "react-native";
+import { useState } from "react";
+import {
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+  FlatList,
+} from "react-native";
 import { styles } from "./styles";
 import Participant from "../../components/Participant";
 
 export default function Home() {
+  const [participants, setParticipants] = useState<string[]>([]);
+  const [participantName, setParticipantName] = useState("");
+
   function handleParticipantAdd() {
-    console.log("Você clicou no botão de adicionar!");
+    if (participants.includes(participantName)) {
+      setParticipantName("");
+      return Alert.alert("Adicionar", "Esse Participante já foi adicionado");
+    }
+    setParticipants((prevState) => [...prevState, participantName]);
+    setParticipantName("");
+  }
+
+  function handleParticipantRemove(name: string) {
+    Alert.alert("Remover", `Remover o participante ${name}?`, [
+      {
+        text: "Não",
+        style: "cancel",
+      },
+      {
+        text: "Sim",
+        onPress: () =>
+          setParticipants((prevState) => [
+            ...prevState.filter((participants) => participants !== name),
+          ]),
+      },
+    ]);
   }
 
   return (
@@ -16,14 +48,33 @@ export default function Home() {
           style={styles.input}
           placeholder="Nome do participante"
           placeholderTextColor="#6b6b6b"
+          onChangeText={setParticipantName}
+          value={participantName}
         />
 
         <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
           <Text style={styles.buttonText}>+</Text>
         </TouchableOpacity>
       </View>
-      <Participant name="Diego Fernandes" />
-      <Participant name="Natanael Melo" />
+
+      <FlatList
+        data={participants}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Participant
+            key={item}
+            name={item}
+            onRemove={() => handleParticipantRemove(item)}
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <Text style={styles.listEmptyText}>
+            Ninguém chegou no evento ainda? Adicione participantes a sua lista
+            de presença.
+          </Text>
+        )}
+      />
     </View>
   );
 }
